933b430d18c9deb0b96bbaa3c89dc912f3f42a30
Prema <165977238+Premasekar30@users.noreply.github.com>
2024-09-22 15:29:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+/*side Navbar*/
+var sideNavbar=document.querySelector(".sidenavbar")
+
+function showNavbar()
+{
+    sideNavbar.style.display="block"
+
+}
+function CloseNavbar(){
+    sideNavbar.style.display="none"
+}
